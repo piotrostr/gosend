c33602ff69f3b5727aa5000ac36957bea9be8e33
piotrostr <piotr.ostrowski@aol.com>
2022-07-17 15:25:55 +0200

    style: clean up, add comments

--- eth/eth.go
+++ eth/eth.go
@@ -18,6 +18,7 @@ import (
 
 var ctx = context.TODO()
 
+// get an environment variable, throw err if does not exist
 func getEnv(variable string) string {
 	val, exists := os.LookupEnv(variable)
 	if !exists {
@@ -28,52 +29,57 @@ func getEnv(variable string) string {
 }
 
 type Eth struct {
-	chainId *big.Int
 	Client  *ethclient.Client
+	chainId *big.Int
 	prv     *ecdsa.PrivateKey
 	address *common.Address
 	balance *big.Int
 }
 
 func (eth *Eth) Init(chainName string) {
-	var Client *ethclient.Client
+	// get client depending on chainName param
+	var client *ethclient.Client
 	var err error
 	switch chainName {
 	case "mainnet":
 	case "rinkeby":
 		rawUrl := "https://%s.infura.io/v3/%s"
 		url := fmt.Sprintf(rawUrl, chainName, getEnv("INFURA_KEY"))
-		Client, err = ethclient.Dial(url)
+		client, err = ethclient.Dial(url)
 		if err != nil {
 			log.Fatal(err)
 		}
 	case "localhost":
-		Client, err = ethclient.Dial("http://localhost:8545")
+		client, err = ethclient.Dial("http://localhost:8545")
 		if err != nil {
 			log.Fatal(err)
 		}
 	default:
 		log.Fatalf("chain %s not supported", chainName)
 	}
-	eth.Client = Client
+	eth.Client = client
 
-	chainId, err := Client.NetworkID(ctx)
+	// get chainId
+	chainId, err := eth.Client.NetworkID(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 	eth.chainId = chainId
 
+	// get private key env var and ecdsa-ify it
 	prv, err := crypto.HexToECDSA(getEnv("PRIVATE_KEY"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	eth.prv = prv
 
+	// get address from pub key
 	address := crypto.PubkeyToAddress(prv.PublicKey)
 	eth.address = &address
 
 	eth.UpdateBalance()
 
+	// print initialized eth struct vals
 	fmt.Println("ChainID:", eth.chainId)
 	fmt.Println("Address:", eth.address.Hex())
 	fmt.Println("Balance:", eth.balance, "wei")
@@ -88,16 +94,19 @@ func (eth *Eth) UpdateBalance() {
 }
 
 func (eth *Eth) Send(to *common.Address, qty *big.Int) {
+	// get nonce
 	nonce, err := eth.Client.PendingNonceAt(ctx, *eth.address)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// get gasPrice
 	gasPrice, err := eth.Client.SuggestGasPrice(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// make the tx struct
 	txdata := &types.LegacyTx{
 		Nonce:    nonce,
 		Gas:      21000,
@@ -106,6 +115,8 @@ func (eth *Eth) Send(to *common.Address, qty *big.Int) {
 		Value:    qty,
 		Data:     []byte{},
 	}
+
+	// sign the tx
 	tx, _ := types.SignNewTx(
 		eth.prv,
 		types.LatestSignerForChainID(eth.chainId),
@@ -115,16 +126,22 @@ func (eth *Eth) Send(to *common.Address, qty *big.Int) {
 		log.Fatal(err)
 	}
 
+	// send the tx
 	err = eth.Client.SendTransaction(ctx, tx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// cooldown (wait for the tx to be added to the block)
+	time.Sleep(time.Second * 1)
+
+	// check if tx pending
 	_, isPending, err := eth.Client.TransactionByHash(ctx, tx.Hash())
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// if tx is pending, let user know and wait for it to be mined
 	if isPending {
 		fmt.Println("Transaction is pending")
 	}
@@ -137,13 +154,15 @@ func (eth *Eth) Send(to *common.Address, qty *big.Int) {
 		time.Sleep(1 * time.Second) // add cooldown var
 	}
 
+	// get the tx receipt
 	rec, err := eth.Client.TransactionReceipt(ctx, tx.Hash())
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// prettify and print to stdout
 	pretty, _ := json.MarshalIndent(rec, "", "  ")
 	fmt.Printf("Receipt: %+v\n", string(pretty))
 
 	eth.UpdateBalance()
-	fmt.Println("PostTxBalance:", eth.balance, "wei")
 }
